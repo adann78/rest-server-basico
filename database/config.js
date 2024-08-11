b2006713ea9bc42dcb59f36cd14a796b3cae5de3
adann78 <adanher78@gmail.com>
2024-08-11 11:08:23 -0700

    Primer commit rest-server

--- database/config.js
+++ database/config.js
@@ -2,8 +2,7 @@ const mongoose=require('mongoose')
 const dbConnection=async()=>{
 try{
 await mongoose.connect( process.env.MONGO_DB_CNN,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    
    
 });
 console.log('Base de datos online');
